50432f0f5e586ec57d13932e56e09f1dc809d582
Praful kumar <105449057+Prafulkumar950@users.noreply.github.com>
2024-09-30 13:19:41 +0530

    Add files via upload

---
+++ PatientRegistration.aspx
@@ -0,0 +1,180 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="PatientRegistration.aspx.cs" Inherits="PatientRegistration" MaintainScrollPositionOnPostback="true"   %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Patient Registration By Praful</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="Container">
+        
+         <div class="img">
+             <img src="icon.png"/>
+         </div>
+
+        <asp:Label ID="lblName" runat="server" Text="Name :"  />
+        <asp:TextBox ID="txtName" runat="server" placeholder="Enter your Name" style="width: 60%;  padding: 5px; border: 2px solid black; border-radius: 10px; background: transparent; font-size: 16px; color:black; margin: 5px 38px;  " ></asp:TextBox>
+        <br/>
+
+        <asp:Label ID="lblAge" runat="server" Text="Age :" />
+        <asp:TextBox ID="txtAge" runat="server" placeholder="Enter your Age" style="width: 60%; padding: 5px; border: 2px solid black ; border-radius: 10px; background: transparent; font-size: 16px; color:black; margin: 5px 52px;  " ></asp:TextBox>
+        <br/>
+
+        <asp:Label ID="lblAddress" runat="server" Text="Address :" />
+        <asp:TextBox ID="txtAddress" runat="server" placeholder="Enter your Address" style="width: 60%; padding: 5px; border: 2px solid black ; border-radius: 10px; background: transparent; font-size: 16px; color:black; margin: 5px 22px;  "></asp:TextBox>
+        <br/>
+
+        <asp:Label ID="lblContact" runat="server" Text="Contact No :" />
+        <asp:TextBox ID="txtContact" runat="server" placeholder="Enter your Contact Number" style="width: 60%; padding: 5px; border: 2px solid black;  border-radius: 10px; background: transparent; font-size: 16px; color:black; margin: 5px 0px;  " ></asp:TextBox>
+        <br/>
+
+        <asp:Label ID="lblDoctor" runat="server" Text="Doctor :"  ></asp:Label>
+        <asp:DropDownList ID="ddlDoctor" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlDoctor_SelectedIndexChanged" style="width: 63%; padding: 5px; border: 2px solid black; cursor:pointer; border-radius: 10px; background: transparent; font-size: 16px; color:black; margin: 5px 34px; "> </asp:DropDownList>
+        <br/>
+
+        <asp:Label ID="lblTreatment" runat="server" Text="Treatment :"  ></asp:Label>
+        <asp:DropDownList ID="ddlTreatment" runat="server"  style="width: 63%; padding: 5px; border: 2px solid black; cursor:pointer; border-radius: 10px; background: transparent; font-size: 16px; color:black; margin: 5px 9px; "> </asp:DropDownList>
+        <br/>
+
+        <%--<asp:Label ID="lblTreatment" runat="server" Text="Treatment :"  ></asp:Label>
+        <asp:DropDownList ID="ddlTreatment" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlTreatment_SelectedIndexChanged"  style="width: 63%; padding: 5px; border: 2px solid black; border-radius: 10px; background: transparent; font-size: 16px; color:black; margin: 5px 9px; "> </asp:DropDownList>
+        <br/>--%>
+
+        <asp:Label ID="lblHospital" runat="server" Text="Hospital :"  ></asp:Label>
+        <asp:DropDownList ID="ddlHospital" runat="server"  style="width: 63%; padding: 5px; border: 2px solid black; cursor:pointer; border-radius: 10px; background: transparent; font-size: 16px; color:black; margin: 5px 23px;  "> </asp:DropDownList>
+        <br/>
+       
+        <asp:HiddenField ID="HiddenF" runat="server" />
+        <asp:Label ID="lblMessage" runat="server" ForeColor="Red"></asp:Label>
+        <asp:Button ID="btnRegister" runat="server" Text="SUBMIT" OnClick="btnRegister_Click"  style="width: 40%; padding: 8px; letter-spacing:4px;  border-radius: 10px;  font-weight:bold; transition:.5s;   font-size: 16px; color:black; margin: 5px  120px; margin-top:40px;   " />        
+       
+        <br/>
+        </div>
+        <br />
+         <asp:Button ID="btnCancel" runat="server" Text="Cancel" OnClick="btnCancel_Click" style=" background-color:red; font-weight:bold; color:black;   border: 1px solid black; border-radius: 3px; text-decoration:none;  margin-left:83%; padding:7px;" />
+       
+       <%-- <div style="margin-left:82%; margin-bottom:1%;">--%>
+            <asp:Button ID="btnDltSelected" runat="server" Text="DELETE " OnClick="btnDltSelected_Click"   style=" background-color:red; font-weight:bold; color:black;   border: 1px solid black; border-radius: 3px; text-decoration:none;  margin-left:92%; padding:7px;" />
+        
+
+
+         <asp:GridView ID="griddetail" runat="server" AutoGenerateColumns="false"  RowStyle-HorizontalAlign="Center" CellPadding="10"  BackColor="LightGrey" AlternatingRowStyle-BackColor="#00ccff"  AllowPaging="true" PageSize="5" OnPageIndexChanging="griddetail_PageIndexChanging" HeaderStyle-BackColor="#00ff00" Width="100%" DataKeyNames="Name, Age, Address, Contact,Id"  >
+             <Columns>
+               <asp:TemplateField HeaderText="S.No." HeaderStyle-CssClass="Header2">
+                   <ItemTemplate>
+                       <asp:Label ID="labelSNo" runat="server" Text="<%#Container.DataItemIndex+1%>"></asp:Label>
+                   </ItemTemplate>
+               </asp:TemplateField>
+
+               <asp:TemplateField HeaderText="Name" >
+                        <ItemTemplate>
+                            <asp:Label ID="labelName" runat="server"  Text='<%# Eval("Name") %>' ></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    
+               <asp:TemplateField HeaderText="Age"  >
+                        <ItemTemplate>
+                            <asp:Label ID="labelAge" runat="server" Text='<%# Eval("Age") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    
+               <asp:TemplateField HeaderText="Address"  >
+                        <ItemTemplate>
+                            <asp:Label ID="labelAddress" runat="server" Text='<%# Eval("Address") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+
+                <asp:TemplateField HeaderText="Contact" >
+                        <ItemTemplate>
+                            <asp:Label ID="labelContact" runat="server"  Text='<%# Eval("Contact") %>' ></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+
+               <asp:TemplateField HeaderText="Doctor" >
+                        <ItemTemplate>
+                            <asp:Label ID="labelDoctor" runat="server"  Text='<%# Eval("Doctor") %>' ></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    
+               <asp:TemplateField HeaderText="Treatment"  >
+                        <ItemTemplate>
+                            <asp:Label ID="labelTreatment" runat="server" Text='<%# Eval("Treatment") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+
+               <asp:TemplateField HeaderText="Hospital"  >
+                        <ItemTemplate>
+                            <asp:Label ID="labelHospital" runat="server" Text='<%# Eval("Hospital") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+
+              <asp:TemplateField HeaderText="Operation">
+                   <ItemTemplate>
+                       <asp:LinkButton ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click"  style=" background-color:red; font-weight:bold; color:black; letter-spacing:1px;  border: 1px solid black; border-radius: 3px; text-decoration:none;" />
+                       <asp:LinkButton ID="btnEdit" runat="server" Text="Edit" OnClick="btnEdit_Click"  style=" background-color:blue; font-weight:bold; color:black; letter-spacing:1px;  border: 1px solid black; border-radius: 3px; text-decoration:none;"  />
+                   </ItemTemplate>
+               </asp:TemplateField>
+
+                 <asp:TemplateField>
+                    <HeaderTemplate>
+                        <asp:CheckBox ID="chkSelectAll" runat="server" Text="Select All" OnCheckedChanged="chkSelectAll_CheckedChanged"  AutoPostBack="true" />
+                    </HeaderTemplate>
+                    <ItemTemplate>
+                        <asp:CheckBox ID="chkSelect" runat="server"  OnCheckedChanged="chkSelect_CheckedChanged" AutoPostBack="true" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+          
+           </Columns>  
+       </asp:GridView>     
+    </form>
+
+        <style>
+            body {
+                font-family: sans-serif;
+                background:url(BCKPAT.jpg)no-repeat;
+                background-position: center;
+                background-size:cover;
+            }
+
+            .Container {
+                width: 100%;
+                max-width: 400px;
+                margin: 80px 200px;
+                padding: 40px;
+                height:100%;
+                background: linear-gradient(255, 255, 255, 0.5);
+                border: 2px solid black;
+                border-radius: 20px;
+                backdrop-filter: blur(50px);
+                box-shadow: 0px 4px 20px black;  
+            }
+
+            .container-img {
+                border-radius: 20px;
+                width: 100%;
+                padding: 5px 0px;
+                margin: 5px 0px;
+            }
+
+            .img {
+                display: flex;
+                justify-content: center;
+                margin-bottom: 30px;
+                margin-top:-10px;
+            }
+             
+            .img img {
+                max-width: 100%;
+                height: 50px;
+                border-radius: 10px;
+            }
+
+            #btnRegister:hover {
+                background-color:lightgreen;
+                cursor:pointer;
+            }
+        </style>
+</body>
+</html>
---
+++ PatientRegistration.aspx.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class PatientRegistration : System.Web.UI.Page
+{
+    DbFunctions objfun = new DbFunctions();
+    DataTable dt = new DataTable();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            objfun.FillDropdownlist(ddlDoctor, "DoctorName", "DoctorId", "SELECT * FROM DoctorTable ", "Select Doctor");
+
+            ddlTreatment.Items.Clear();
+            ddlTreatment.Items.Add(new ListItem("Select Treatment", "0"));
+
+            ddlHospital.Items.Clear();
+            ddlHospital.Items.Add(new ListItem("Select Hospital", "0"));
+            Grid();
+            btnDltSelected.Visible = false;
+            btnCancel.Visible = false;
+        }
+       
+        lblMessage.Text = "";
+    }
+
+    protected void ddlDoctor_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (ddlDoctor.SelectedValue != "0")
+        {
+            //objfun.FillDropdownlist(ddlTreatment, "TreatmentName", "TreatmentId", "SELECT * FROM TreatmentTable WHERE DoctorId = '" + ddlDoctor.SelectedValue + "'", "Select Treatment ");
+
+            string TID = objfun.Get_details("SELECT TreatmentId FROM DoctorTable WHERE DoctorId = '" + ddlDoctor.SelectedValue + "'");
+            objfun.FillDropdownlist(ddlTreatment, "TreatmentName", "TreatmentId", "SELECT TreatmentId, TreatmentName FROM TreatmentTable WHERE TreatmentId = '" + TID + "'", "");
+
+            string HID = objfun.Get_details("SELECT HospitalId FROM TreatmentTable WHERE TreatmentId = '" + TID + "'");
+            objfun.FillDropdownlist(ddlHospital, "HospitalName", "HospitalId", "SELECT HospitalId, HospitalName FROM HospitalTable WHERE HospitalId = '" + HID + "'", "");
+        }
+        else
+        {
+            ddlTreatment.Items.Clear();
+            ddlTreatment.Items.Add(new ListItem("Select Treatment", "0"));
+
+            ddlHospital.Items.Clear();
+            ddlHospital.Items.Add(new ListItem("Select Hospital", "0"));
+        }
+    }
+
+    //protected void ddlTreatment_SelectedIndexChanged(object sender, EventArgs e)
+    //{
+    //    if (ddlTreatment.SelectedValue != "0")
+    //    {
+    //        objfun.FillDropdownlist(ddlHospital, "HospitalName", "HospitalId", "SELECT * FROM HospitalTable WHERE TreatmentId = '" + ddlTreatment.SelectedValue + "'", "Select Hospital");
+
+    //    }
+    //    else
+    //    {
+
+    //        ddlTreatment.Items.Clear();
+    //        ddlTreatment.Items.Add(new ListItem("Select Treatment", "0"));
+    //    }
+    //}
+
+    
+
+    public void Grid()
+    {
+        dt = objfun.FillDataTable("SELECT  * FROM PatientRegistration order by Id DESC  ");
+        if (dt.Rows.Count > 0)
+        {
+            griddetail.DataSource = dt;
+            griddetail.DataBind();
+
+        }
+        else
+        {
+            griddetail.DataSource = null;
+            griddetail.DataBind();
+
+        }
+    }
+
+    protected void griddetail_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        griddetail.PageIndex = e.NewPageIndex;
+        Grid();
+    }
+
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        LinkButton btnDelete = (LinkButton)sender;
+        GridViewRow row = (GridViewRow)btnDelete.NamingContainer;
+        int rowIndex = row.RowIndex;
+
+        //string id = (griddetail.DataKeys[rowIndex].Values["Name"].ToString());
+        int id = Convert.ToInt32(griddetail.DataKeys[rowIndex].Values["Id"]);
+       // objfun.Fill_FormDeleteDML(id);
+        objfun.ExecuteDML("DELETE  FROM PatientRegistration  WHERE Id = '" + id + "'");
+        //objfun.ExecuteDML("DELETE  FROM PatientRegistration  WHERE Name = '" + id + "'");
+
+        Grid();
+        objfun.MsgBox("Record Deleted SuccessFully", this);
+    }
+
+    protected void btnEdit_Click(object sender, EventArgs e)
+    {
+
+        LinkButton btnEdit = (LinkButton)sender;
+        GridViewRow row = (GridViewRow)btnEdit.NamingContainer;
+        int rowIndex = row.RowIndex;
+        int id = Convert.ToInt32(griddetail.DataKeys[rowIndex].Values ["Id"]);
+        DataTable dt = objfun.FillDataTable("SELECT Name, Age, Address, Contact, Doctor, Treatment, Hospital FROM PatientRegistration WHERE Id = '" + id + "'");
+        if (dt.Rows.Count > 0)
+        {
+            string name = dt.Rows[0]["Name"].ToString();
+            string age = dt.Rows[0]["Age"].ToString();
+            string address = dt.Rows[0]["Address"].ToString();
+            string contact = dt.Rows[0]["Contact"].ToString();
+            string doctor = dt.Rows[0]["Doctor"].ToString();
+            string treatment = dt.Rows[0]["Treatment"].ToString();
+            string hospital = dt.Rows[0]["Hospital"].ToString();
+
+            txtName.Text = name;
+            txtAge.Text = age;
+            txtAddress.Text = address;
+            txtContact.Text = contact;
+
+            objfun.FillDropdownlist(ddlDoctor, "DoctorName", "DoctorId", "SELECT * FROM DoctorTable", "Select Doctor");
+            ddlDoctor.SelectedValue = objfun.Get_details("SELECT DoctorId FROM DoctorTable WHERE DoctorName = '" + doctor + "'");
+
+            string TId = objfun.Get_details("SELECT TreatmentId FROM DoctorTable WHERE DoctorName = '" + doctor + "'");
+            objfun.FillDropdownlist(ddlTreatment, "TreatmentName", "TreatmentId", "SELECT * FROM TreatmentTable WHERE TreatmentId = '" + TId + "'", "Select Treatment");
+            ddlTreatment.SelectedValue = TId;
+
+            string CId = objfun.Get_details("SELECT HospitalId FROM TreatmentTable WHERE TreatmentId = '" + TId + "'");
+            objfun.FillDropdownlist(ddlHospital, "HospitalName", "HospitalId", "SELECT * FROM HospitalTable WHERE HospitalId = '" + CId + "'", "Select Hospital");
+            ddlHospital.SelectedValue = CId;
+
+            HiddenF.Value = id.ToString();
+            btnRegister.Text = "UPDATE";
+            btnCancel.Visible = true;
+           
+
+        }
+    }
+    protected void btnCancel_Click(object sender, EventArgs e)
+    {
+        ResetForm();
+        btnRegister.Text = "REGISTER";
+        btnCancel.Visible = false;
+    }
+
+
+    protected void btnRegister_Click(object sender, EventArgs e)
+    {
+        // Check if already exists
+        string checkQuery = ("SELECT COUNT (*) FROM PatientRegistration WHERE  Name = '" + txtName.Text + "' AND Age = '" + txtAge.Text + "' AND Address = '" + txtAddress.Text + "' AND Contact = '" + txtContact.Text + "' AND Doctor = '" + ddlDoctor.SelectedItem.Text + "' AND Treatment = '" + ddlTreatment.SelectedItem.Text + "' AND Hospital = '" + ddlHospital.SelectedItem.Text + "' ");
+        int count = Convert.ToInt32(objfun.Get_details(checkQuery));
+        if (count > 0)
+        {
+            lblMessage.Text = "Selected Record is already exists.";
+        }
+        else
+        {
+            if (btnRegister.Text == "UPDATE")
+            {
+                int id = Convert.ToInt32(HiddenF.Value);
+                int update = objfun.ExecuteDML("UPDATE PatientRegistration SET Name = '" + txtName.Text + "', Age = '" + txtAge.Text + "', Address = '" + txtAddress.Text + "', Contact = '" + txtContact.Text + "', Doctor = '" + ddlDoctor.SelectedItem.Text + "', Treatment = '" + ddlTreatment.SelectedItem.Text + "', Hospital = '" + ddlHospital.SelectedItem.Text + "' WHERE Id ='" + id + "'");
+                //int update = objfun.Fill_FormUpdateDML( txtName.Text, int.Parse(txtAge.Text), txtAddress.Text, txtContact.Text, ddlDoctor.SelectedItem.Text, ddlTreatment.SelectedItem.Text, ddlHospital.SelectedItem.Text, id);
+                btnCancel.Visible = false;
+                btnRegister.Text = "REGISTER";
+                Grid();
+                objfun.MsgBox("Record Updated SuccessFully", this);
+            }
+            else
+            {
+                 int save = objfun.ExecuteDML("INSERT INTO PatientRegistration (Name, Age, Address, Contact, Doctor, Treatment, Hospital) VALUES ('" + txtName.Text + "','" + txtAge.Text + "','" + txtAddress.Text + "','" + txtContact.Text + "','" + ddlDoctor.SelectedItem.Text + "','" + ddlTreatment.SelectedItem.Text + "','" + ddlHospital.SelectedItem.Text + "')");
+               //int save = objfun.Fill_FormDML( txtName.Text, Convert.ToInt32(txtAge.Text), txtAddress.Text, txtContact.Text, ddlDoctor.SelectedItem.Text, ddlTreatment.SelectedItem.Text, ddlHospital.SelectedItem.Text);
+                    
+                    
+                    //int save = objfun.Fill_FormDML();
+                Grid();
+                objfun.MsgBox("Record Stored SuccessFully", this);
+            }
+            ResetForm();
+        }
+    }
+
+    private void ResetForm()
+    {
+        txtName.Text = "";
+        txtAge.Text = "";
+        txtAddress.Text = "";
+        txtContact.Text = "";
+        ddlDoctor.SelectedIndex = 0;
+        
+        HiddenF.Value = "";
+       
+        ddlTreatment.Items.Clear();
+        ddlTreatment.Items.Add(new ListItem("Select Treatment", "0"));
+
+        ddlHospital.Items.Clear();
+        ddlHospital.Items.Add(new ListItem("Select Hospital", "0"));
+    }
+
+
+
+
+    protected void chkSelectAll_CheckedChanged(object sender, EventArgs e)
+    {
+
+        CheckBox chkAll = (CheckBox)sender;
+        bool allChecked = chkAll.Checked;
+
+        foreach (GridViewRow row in griddetail.Rows)
+        {
+            CheckBox chk = (CheckBox)row.FindControl("chkSelect");
+            if (chk != null)
+            {
+                chk.Checked = allChecked;
+            }
+        }
+
+        btnDltSelected.Visible = allChecked;
+    }
+        
+    
+
+    protected void chkSelect_CheckedChanged(object sender, EventArgs e)
+    {
+
+        bool anyChecked = false;
+        foreach (GridViewRow row in griddetail.Rows)
+        {
+            CheckBox chk = (CheckBox)row.FindControl("chkSelect");
+            if (chk != null && chk.Checked)
+            {
+                anyChecked = true;
+                break;
+            }
+        }
+        btnDltSelected.Visible = anyChecked;
+
+
+
+        bool allChecked = true;
+        foreach (GridViewRow row in griddetail.Rows)
+        {
+            CheckBox chk = (CheckBox)row.FindControl("chkSelect");
+            if (chk != null && !chk.Checked)
+            {
+                allChecked = false;
+                break;
+            }
+        }
+
+        CheckBox chkSelectAll = (CheckBox)griddetail.HeaderRow.FindControl("chkSelectAll");
+        if (chkSelectAll != null)
+        {
+            chkSelectAll.Checked = allChecked;
+        }
+        
+    }
+    
+    
+    protected void btnDltSelected_Click(object sender, EventArgs e)
+    {
+        bool anySelected = false;
+        List<string> deletedData = new List<string>();
+
+
+        foreach (GridViewRow row in griddetail.Rows)
+        {
+            CheckBox chk = (CheckBox)row.FindControl("chkSelect");
+            
+           
+            if (chk != null && chk.Checked)
+            {
+                anySelected = true;
+                int id = Convert.ToInt32(griddetail.DataKeys[row.RowIndex].Values["Id"]);
+               
+                  DataTable dt = objfun.FillDataTable("SELECT * FROM PatientRegistration WHERE Id = '" + id + "'");
+                    if (dt.Rows.Count > 0)
+                    {
+                        string name = dt.Rows[0]["Name"].ToString();
+                        string age = dt.Rows[0]["Age"].ToString();
+                        string address = dt.Rows[0]["Address"].ToString();
+                        string contact = dt.Rows[0]["Contact"].ToString();
+
+                        objfun.ExecuteDML("DELETE FROM PatientRegistration WHERE Id = '" + id + "'");
+
+                        deletedData.Add(name);
+                        deletedData.Add(age);
+                        deletedData.Add(address);
+                        deletedData.Add(contact);
+
+                    }
+            }
+        }
+
+
+        if (anySelected)
+        {
+            if (deletedData.Count > 0)
+            {
+                string data = string.Join(" | ", deletedData);
+                objfun.MsgBox("Records Deleted Successfully: " + data, this);
+
+            }
+        }
+        else
+        {
+            objfun.MsgBox("No data is selected", this);
+        }
+
+        Grid();
+        btnDltSelected.Visible = false;
+
+    }
+}
+
+
+
+    
